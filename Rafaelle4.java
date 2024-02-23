4e1c25836db296314532579eb088303d7d19d34a
Faelly <152607054+EllyFaelly@users.noreply.github.com>
2024-02-23 14:22:54 -0300

    Update Rafaelle4.java

--- Rafaelle4.java
+++ Rafaelle4.java
@@ -1,34 +1,31 @@
-
-package rafaelle4;
+package atividade1;
 
 import java.util.Scanner;
 
 public class Rafaelle4 {
 
-    
+   
     public static void main(String[] args) {
-        int number = 0;
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um valor inteiro maior ou igual a 0: ");
-        int valor = scanner.nextInt();
-   
-        if (valor < 0) {
-            System.out.println("Digite um valor não negativo!!");
-        } else if (valor == 0) {
-            number = 1;
-            System.out.println("O número de dígitos do número que você digitou é: " + number);
-        } else {
-            while (valor > 0) {
-                valor /= 10;
-                number++;
-            }
-            System.out.println("O número de dígitos do número que você digitou é: " + number);
-        }
-
+        String valor = scanner.nextLine();
         
-        scanner.close();
-
-   }       
-    }
+        try {  
+            int valorNumero = Integer.parseInt(valor);  
+            
+            if (valorNumero >= 0)
+            {
+                int numeroDeDigitos = valor.length();
+                System.out.println("A quantidade de dígitos do número é:" + numeroDeDigitos);
+                return;
+            }
+            
+            System.out.println("[E]: O VALOR DEVE SER MAIOR OU IGUAL A ZERO");
+            
+        } catch(NumberFormatException e){
+            System.out.println("[E]: O VALOR DEVE SER UM NÚMERO");
+        }  
+    }    
     
-
+}
